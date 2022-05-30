86e32639a84f34b7ad720c852cfdc9fdd21a8e60
Garrett Adams <garrett.adams52@gmail.com>
2022-05-30 18:42:49 -0400

    finished js

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,18 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
+//const pad = (array, minSize, value=null) => {}
+
+function padArray(arr, num, pad = 'None'){
+
+    let diff = num - arr.length
+
+    while(diff>0){
+        arr.push(pad)
+        diff--
+    }
+
+    return arr
 
 }
+
+console.log(padArray([1,2,3], 2, 'apple'))
+console.log(padArray([1,2,3], 2))
